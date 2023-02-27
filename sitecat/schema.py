11a5fe00ce12ec3e2f113cc5fc8c6992cc995b36
AmirRazakInfin8 <amir.razak@infin8co.com>
2023-02-27 12:11:33 +0800

    added working mutations

--- sitecat/schema.py
+++ sitecat/schema.py
@@ -82,21 +82,62 @@ class HumanMutation(graphene.Mutation):
         description = graphene.String()
         home_name = graphene.String(required=True)
 
+    human = graphene.Field(HumanType)
+
     @classmethod
-    def mutate(cls, root, info, name, gender, birth_date, description, home_name):
-        human = Human.objects.get(pk=id)
+    def mutate(cls, root, info, id,  name, gender, birth_date, description, home_name):
+        human = Human.objects.get(id=id)
         human.name = name
         human.gender = gender
         human.birth_date = birth_date
         human.description = description
         human.home = Home.objects.get(name=home_name)
         human.save()
+        print(human)
 
         return HumanMutation(human=human)
 
 
+class HumanMutateName(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String(required=True)
+
+    human = graphene.Field(HumanType)
+
+    @classmethod
+    def mutate(cls, root, info, name, id):
+        human = Human.objects.get(id=id)
+        human.name = name
+        human.save()
+
+        return HumanMutateName(human=human)
+
+
+class HomeMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String(required=True)
+        address = graphene.String(required=True)
+        house_type = graphene.String(required=True)
+
+    home = graphene.Field(HomeType)
+
+    @classmethod
+    def mutate(cls, root, info, id, name, address, house_type):
+        home = Home.objects.get(id=id)
+        home.name = name
+        home.address = address
+        home.house_type = house_type
+        home.save()
+
+        return HomeMutation(home=home)
+
+
 class Mutation(graphene.ObjectType):
     update_human = HumanMutation.Field()
+    update_home = HomeMutation.Field()
+    update_human_name = HumanMutateName.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
