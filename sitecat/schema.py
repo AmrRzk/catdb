6a6910c2a5da921284f783f00dc334306d744c0b
AmirRazakInfin8 <amir.razak@infin8co.com>
2023-02-27 13:01:22 +0800

    class human

--- sitecat/schema.py
+++ sitecat/schema.py
@@ -73,29 +73,41 @@ class Query(graphene.ObjectType):
         return Breed.objects.get(id=id)
 
 
-class HumanMutation(graphene.Mutation):
-    class Arguments:
-        id = graphene.ID()
-        name = graphene.String(required=True)
-        gender = graphene.String(required=True)
-        birth_date = graphene.Date(required=True)
-        description = graphene.String()
-        home_name = graphene.String(required=True)
+class HumanSchema:
+    class Mutation(graphene.Mutation):
+        class Arguments:
+            id = graphene.ID()
+            name = graphene.String(required=True)
+            gender = graphene.String(required=True)
+            birth_date = graphene.Date(required=True)
+            description = graphene.String()
+            home_name = graphene.String(required=True)
 
-    human = graphene.Field(HumanType)
+        human = graphene.Field(HumanType)
 
-    @classmethod
-    def mutate(cls, root, info, id,  name, gender, birth_date, description, home_name):
-        human = Human.objects.get(id=id)
-        human.name = name
-        human.gender = gender
-        human.birth_date = birth_date
-        human.description = description
-        human.home = Home.objects.get(name=home_name)
-        human.save()
-        print(human)
+        @classmethod
+        def mutate(cls, root, info, id,  name, gender, birth_date, description, home_name):
+            human = Human.objects.get(id=id)
+            human.name = name
+            human.gender = gender
+            human.birth_date = birth_date
+            human.description = description
+            human.home = Home.objects.get(name=home_name)
+            human.save()
+            print(human)
 
-        return HumanMutation(human=human)
+            return HumanSchema.Mutation(human=human)
+
+    class Delete(graphene.Mutation):
+        class Arguments:
+            id = graphene.ID()
+        human = graphene.Field(Human)
+
+        @classmethod
+        def mutate(cls, root, info, id):
+            human = Human.objects.get(id=id)
+            human.delete()
+            return HumanSchema.Delete(human=human)
 
 
 class HomeMutation(graphene.Mutation):
@@ -116,6 +128,53 @@ class HomeMutation(graphene.Mutation):
         home.save()
 
         return HomeMutation(home=home)
+
+
+class BreedMutation():
+
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String(required=True)
+        origin = graphene.String(required=True)
+        description = graphene.String(required=True)
+
+    breed = graphene.Field(BreedType)
+
+    @classmethod
+    def mutate(cls, root, info, id, name, origin, description):
+        breed = Breed.objects.get(id=id)
+        breed.name = name
+        breed.origin = origin
+        breed.description = description
+        breed.save()
+
+        return BreedMutation(breed=breed)
+
+
+class CatMutation():
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String(required=True)
+        gender = graphene.String(required=True)
+        birth_date = graphene.Date(required=True)
+        description = graphene.String()
+        breed_name = graphene.String()
+        owner_name = graphene.String()
+
+    cat = graphene.Field(CatType)
+
+    @classmethod
+    def mutate(cls, root, info, id, name, gender, birth_date, description, breed_name, owner_name):
+        cat = Cat.objects.get(id=id)
+        cat.name = name
+        cat.gender = gender
+        cat.birth_date = birth_date
+        cat.description = description
+        cat.breed = Breed.objects.get(name=breed_name)
+        cat.owner = Human.objects.get(name=owner_name)
+        cat.save()
+
+        return CatMutation(cat=cat)
 
 
 class Mutation(graphene.ObjectType):
