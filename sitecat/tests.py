adedfca173d6dabd90f46ed18bb36559f5d9c38b
AmirRazakInfin8 <amir.razak@infin8co.com>
2023-03-04 16:34:34 +0800

    More tests

--- sitecat/tests.py
+++ sitecat/tests.py
@@ -56,7 +56,7 @@ class APITestCase(TestCase):
 
     def test_allhumans(self):
         query = """
-        query {
+        query allHumans{
             allHumans {
                 name
                 home {
@@ -66,6 +66,7 @@ class APITestCase(TestCase):
         }
         """
         result = schema.execute(query)
+
         assert_query = [{
             "name": "Bryan",
             "home": {
@@ -82,7 +83,7 @@ class APITestCase(TestCase):
 
     def test_allhumans_filter(self):
         query = """
-        query {
+        query AllHumansWithFilter{
             allHumans(name_Icontains:"Mark") {
                 name
                 home {
@@ -92,10 +93,147 @@ class APITestCase(TestCase):
         }
         """
         result = schema.execute(query)
+
         assert_query = [{
             "name": "Mark Study",
             "home": {
                 "name": "Cozy Home"
             }
         }]
+
         self.assertEqual(result.data["allHumans"], assert_query)
+
+    def test_allhumans_create(self):
+        query = """
+        mutation CreateHuman{
+            createOrUpdateHuman(input: {
+                name: "Richard Lee",
+                description: "Very rich person",
+                gender: "M",
+                birthDate: "1970-01-21",
+                home: {
+                    name: "Fancy Sky",
+                    address: "24, Rich Street",
+                    houseType: "CONDOMINIUM",
+                }
+            }) {
+                name
+                description
+                home {
+                    name
+                }
+            }
+        }        
+        """
+
+        result = schema.execute(query)
+
+        assert_query = {
+            "name": "Richard Lee",
+            "description": "Very rich person",
+            "home": {
+                "name": "Fancy Sky"
+            }
+        }
+
+        self.assertEqual(result.data["createOrUpdateHuman"], assert_query)
+
+    def test_updatehumans(self):
+        init_query = """
+        query listHuman {
+            allHumans {
+                id
+            }
+        }
+        """
+
+        init_res = schema.execute(init_query)
+        human_id = init_res.data["allHumans"][0]["id"]
+
+        query = """
+        mutation UpdateHuman{
+            createOrUpdateHuman(input: { """ + f" id: {human_id}," + """
+                name: "Bryan",
+                description: "He is now poor",
+                home: {
+                    name: "Homeless",
+                    address: "In the streets",
+                    houseType: "FLAT"
+                }
+            }) {
+                name
+                description
+                home {
+                    name
+                }
+            }
+        }
+        """
+
+        result = schema.execute(query)
+
+        assert_query = {
+            "name": "Bryan",
+            "description": "He is now poor",
+            "home": {
+                "name": "Homeless"
+            }
+        }
+
+        self.assertEqual(result.data["createOrUpdateHuman"], assert_query)
+
+    def test_deletehuman(self):
+        init_query = """
+        query listHuman {
+            allHumans {
+                id
+            }
+        }
+        """
+
+        init_res = schema.execute(init_query)
+        human_id = init_res.data["allHumans"][1]["id"]
+        query = """
+         mutation DeleteHuman{
+            deleteHuman""" + f"(id: {human_id})" + """ {
+                ok
+            }
+         }
+         """
+
+        result = schema.execute(query)
+
+        assert_query = {
+            "ok": True
+        }
+
+        self.assertEqual(result.data["deleteHuman"], assert_query)
+
+    def test_allCats(self):
+        query = """
+        query AllCats {
+            allCats {
+                name
+                owner {
+                    name
+                }
+                breed {
+                    name
+                }
+            }
+        }
+        """
+
+        result = schema.execute(query)
+
+        assert_query = [{
+            "name": "Fluff",
+            "owner": {
+                "name": "Bryan",
+            },
+            "breed": {
+                "name": "Schrodingers"
+            }
+        }]
+
+        self.assertEqual(result.data["allCats"], assert_query)
