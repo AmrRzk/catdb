0990038d14dc0aed3044beba1ab82b5aef170342
AmirRazakInfin8 <amir.razak@infin8co.com>
2023-03-14 14:34:23 +0800

    Setup celery

--- cat/settings.py
+++ cat/settings.py
@@ -171,3 +171,6 @@ ELASTICSEARCH_DSL = {
         "hosts": "es:9200"
     }
 }
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
